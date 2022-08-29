5439c87ea7a956ea042f140a96b7534145b31545
victhall <victoriahall.dev@gmail.com>
2022-08-29 14:04:31 -0400

    add styling to juices summary component

--- src/Components/Juices/JuicesSummary.js
+++ src/Components/Juices/JuicesSummary.js
@@ -2,9 +2,22 @@ import classes from './JuicesSummary.module.css'
 
 export default function JuicesSummary() {
   return (
-   <section className={classes.summary}>
-    <h2>Daily Rituals for Conscious Living</h2>
-    <p>a cold-pressed organic juice company, helping people achieve happier, healthier and wealthier lives.</p>
-   </section>
+    <section className={classes.summary}>
+
+      <div className={classes['left-div']}>
+        <div>
+        <h2>SUSTAINABLE JUICE PRODUCTION &amp; BOTANICAL NUTRITION</h2>
+        </div>
+
+        <div>
+        <h3>Daily Rituals for Conscious Living</h3>
+        </div>
+      </div>
+
+      <div className={classes['right-div']}>
+        <p>a cold-pressed organic juice company, helping people achieve happier, healthier and wealthier lives. We are dedicated to using fresh and organic ingredients so that we can offer the highest quality of food and juice available.</p>
+      </div>
+
+    </section>
   )
 }
