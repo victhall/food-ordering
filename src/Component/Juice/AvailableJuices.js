c184dda086c0cc1b0670a16cd3870c80474f1fc8
victhall <victoriahall.dev@gmail.com>
2022-08-29 11:51:49 -0400

    create drink components

---
+++ src/Component/Juice/AvailableJuices.js
@@ -0,0 +1,48 @@
+
+const DUMMY_JUICES = [
+  {
+    id: 'j1',
+    name: 'After Party',
+    description: 'activated charcoal - alkaline water - lemon - maple syrup - cayenne',
+    size: '250ml',
+    price: 7.50
+  },
+  {
+    id: 'j2',
+    name: 'Blu Majik',
+    description: 'blu majik algae - pineapple - stevia - lime - alkaline water',
+    size: '250ml',
+    price: 7.50
+  },
+  {
+    id: 'j3',
+    name: 'The Good',
+    description: 'Cucumber, Green Lettuce, Spinach, Celery, Lemon (All Organic + Cold-Pressed), Organic Fermented Lemongrass, Himalayan Salt',
+    size: '250ml',
+    price: 7.50
+  },
+  {
+    id: 'j4',
+    name: 'Genius',
+    description: 'Cucumber, Pineapple, Apple, Celery, Spinach, Kale, Lemon (All Organic + Cold-Pressed), Organic Fermented Lemongrass, Himalayan Salt',
+    size: '250ml',
+    price: 7.50,
+  },
+  {
+    id: 'j5',
+    name: 'Farma-C',
+    description: 'Grapefruit, Orange, Lemon (All Organic + Cold-Pressed), Organic Fermented Lemongrass, Cayenne Extract',
+    size: '250ml',
+    price: 7.50,
+  }
+];
+
+export default function AvailableJuices() {
+  const drinksList = DUMMY_JUICES.map(drink => <li>{drink.name}</li>)
+
+  return <section>
+    <ul>
+      {drinksList}
+    </ul>
+  </section>
+}
---
+++ src/Component/Juice/DrinksSummary.js
@@ -0,0 +1,10 @@
+import classes from './JuicesSummary.module.css'
+
+export default function JuicesSummary() {
+  return (
+   <section className={classes.summary}>
+    <h2>Daily Rituals for Conscious Living</h2>
+    <p>a cold-pressed organic juice company, helping people achieve happier, healthier and wealthier lives.</p>
+   </section>
+  )
+}
